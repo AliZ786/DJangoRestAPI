52a4936baf28b923bd2c4854552d85aedabbb060
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2021-05-31 01:20:40 -0400

    Changed settings.py file

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'profiles_api'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
