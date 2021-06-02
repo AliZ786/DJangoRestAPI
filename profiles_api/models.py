de9eca8eafeed4675077b7ed64f36964d02697b0
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2021-06-02 15:19:59 -0400

    Added feed api

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 
 
@@ -58,4 +59,17 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """Returns string representation of the user"""
         return self.email
 
-   
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+       """Returns the models as a string"""
+       return self.status_text
+    
