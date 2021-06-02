e9ce00e6aeaa9d2cb1e63b541a353354fa07710a
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2021-06-02 15:52:12 -0400

    Added permissions to user profile feed

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -12,3 +12,15 @@ class UpdateOwnProfile(permissions.BasePermission):
 
 
         return obj.id == request.user.id    
+
+
+class UpdateOWnStatus(permissions.BasePermission):
+    """Allows user to update their own status"""
+    
+
+    def has_object_permission(self, request, view, obj):
+        """Check if the user is trying to update their status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id       
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,9 @@ from rest_framework import viewsets, filters
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
+
+
 
 
 
@@ -116,6 +119,8 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOWnStatus,IsAuthenticated)
+    
 
 
 
