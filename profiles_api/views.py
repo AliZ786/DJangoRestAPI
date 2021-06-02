32dbc4409ce5434584a7558acefcd24f0bd6a588
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2021-06-02 01:50:50 -0400

    Added creating a user profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
+from profiles_api import serializers, models
 from rest_framework import viewsets
 
 class HelloAPIView(APIView):
@@ -87,4 +87,10 @@ class HelloViewSet(viewsets.ViewSet):
 
     def destroy(self, request, pk = None):
         """Handles removing an object"""
-        return Response({'http_method': 'DELETE'})      
+        return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
